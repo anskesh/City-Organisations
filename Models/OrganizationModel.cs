dc842a2dbe497fd15fc6f66dc0dad8bccb600cb7
Roman Akimov <frbhjvf@yandex.ru>
2024-04-07 11:42:53 +0300

    Модель Организации, Словаря ресурсов, изменение HomePage

---
+++ Models/OrganizationModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CityOrganisations.Models
+{
+    public class OrganizationModel
+    {
+        public string OrganizationName { get; set; }
+        public ObservableCollection<BranchModel> Branches { get; set; }
+
+        public OrganizationModel(string organizationName)
+        {
+            OrganizationName = organizationName;
+            Branches = new ObservableCollection<BranchModel>();
+        }
+    }
+}
--- ViewModels/HomePageViewModel.cs
+++ ViewModels/HomePageViewModel.cs
@@ -1,9 +1,47 @@
-﻿using Prism.Mvvm;
+﻿using CityOrganisations.Models;
+using Prism.Mvvm;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace CityOrganisations.ViewModels
 {
     public class HomePageViewModel : BindableBase
     {
-        
+        private IEnumerable<string> _allBranches;
+
+        public ObservableCollection<OrganizationModel> Organizations { get; set; }
+
+        public HomePageViewModel()
+        {
+            Organizations = new ObservableCollection<OrganizationModel>();
+
+            var organization1 = new OrganizationModel("ЧГУ");
+            var organization2 = new OrganizationModel("Магнит");
+
+            for (int i = 0; i < 10; i++)
+            {
+                organization1.Branches.Add(new BranchModel($"Филиал {i + 1}"));
+                organization2.Branches.Add(new BranchModel($"Филиал {i + 11}"));
+            }
+
+            Organizations.Add(organization1);
+            Organizations.Add(organization2);
+        }
+
+        public IEnumerable<string> GetAllBranches()
+        {
+            foreach (var organization in Organizations)
+            {
+                foreach (var branch in organization.Branches)
+                {
+                    yield return $"{organization.OrganizationName} {branch.Name}";
+                }
+            }
+        }
+
+        public IEnumerable<string> AllBranches
+        {
+            get { return _allBranches ?? (_allBranches = GetAllBranches()); }
+        }
     }
 }
