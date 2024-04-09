7ace110a1bcb6391f99bd48dab93718150ccdd63
Roman Akimov <frbhjvf@yandex.ru>
2024-04-09 22:55:32 +0300

    Масштабирование карты

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace CityOrganisations.Views
 {
@@ -7,6 +11,31 @@ namespace CityOrganisations.Views
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (sender is Border border)
+            {
+                double currentScaleX = border.LayoutTransform.Value.M11;
+                double currentScaleY = border.LayoutTransform.Value.M22;
+
+                /*
+                e.Delta - изменение колеса мыши. Если e.Delta положительное, то это значит,
+                что колесо было прокручено вверх, а если отрицательное, то вниз
+                */
+                double scaleChange = e.Delta > 0 ? 0.1 : -0.1;
+
+                double newScaleX = Math.Max(0.1, currentScaleX + scaleChange);
+                double newScaleY = Math.Max(0.1, currentScaleY + scaleChange);
+
+                border.LayoutTransform = new ScaleTransform(newScaleX, newScaleY);
+
+                double newWidth = MapBorder.ActualWidth + newScaleX;
+                double newHeight = MapBorder.ActualHeight + newScaleY;
+                MapBorder.Width = newWidth;
+                MapBorder.Height = newHeight;
+            }
         }
     }
 }
