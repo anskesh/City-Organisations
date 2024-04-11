5f1c2c23b66be8d14b8070a8d64b4c3dd09a32af
Akimov Roman <frbhjvf@yandex.ru>
2024-04-11 15:07:33 +0300

    Строка поиска филиалов и всплывающее окно фильтра филиалов

--- ViewModels/HomePageViewModel.cs
+++ ViewModels/HomePageViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using CityOrganisations.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace CityOrganisations.ViewModels
 {
     public class HomePageViewModel : BindableBase
     {
         private IEnumerable<string> _allBranches;
+        private bool _isFilterPopupOpen;
 
         public ObservableCollection<OrganizationModel> Organizations { get; set; }
 
@@ -28,6 +31,7 @@ namespace CityOrganisations.ViewModels
             Organizations.Add(organization2);
         }
 
+        // получение списка из всех организаций и всех филиалов для вывода в интерфейс
         public IEnumerable<string> GetAllBranches()
         {
             foreach (var organization in Organizations)
@@ -42,6 +46,31 @@ namespace CityOrganisations.ViewModels
         public IEnumerable<string> AllBranches
         {
             get { return _allBranches ??= GetAllBranches(); }
+        }
+
+        // обработка нажатия на "фильтр" для всплывающего окна
+        public bool IsFilterPopupOpen
+        {
+            get => _isFilterPopupOpen;
+            set => SetProperty(ref _isFilterPopupOpen, value);
+        }
+
+        private DelegateCommand _openFilterCommand;
+        public DelegateCommand OpenFilterCommand =>
+            _openFilterCommand ??= new DelegateCommand(ExecuteOpenFilterCommand);
+
+        private void ExecuteOpenFilterCommand()
+        {
+            IsFilterPopupOpen = true;
+        }
+
+        private DelegateCommand _applyFilterCommand;
+        public DelegateCommand ApplyFilterCommand =>
+            _applyFilterCommand ??= new DelegateCommand(ExecuteApplyFilterCommand);
+
+        private void ExecuteApplyFilterCommand()
+        {
+            IsFilterPopupOpen = false;
         }
     }
 }
--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
