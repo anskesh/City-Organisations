60e3e4fbf00ccec18060324a5288b32f63be3fe2
Akimov Roman <frbhjvf@yandex.ru>
2024-04-22 12:27:45 +0300

    Корректное отображение списка филилов (ошибки)
    - Списки филилов корректн отображаются на HomePage и BranchePage
    - Кнопка "фильтр" в списке филиалов не работает на страницах

--- ViewModels/BranchesPageViewModel.cs
+++ ViewModels/BranchesPageViewModel.cs
@@ -1,15 +1,32 @@
 ﻿using CityOrganisations.Models;
+using Prism.Commands;
 using Prism.Mvvm;
+using System.Collections.Generic;
 
 namespace CityOrganisations.ViewModels
 {
     public class BranchesPageViewModel : BindableBase
     {
+        private FilialBorderViewModel _filialBorderViewModel;
         public BranchModel Branch { get; set; }
 
         public BranchesPageViewModel()
         {
+            _filialBorderViewModel = new FilialBorderViewModel();
             Branch = new BranchModel("Первая компания");
         }
+
+        public IEnumerable<string> AllBranches => _filialBorderViewModel.AllBranches;
+
+        public bool IsFilterPopupOpen
+        {
+            get => _filialBorderViewModel.IsFilterPopupOpen;
+            set => _filialBorderViewModel.IsFilterPopupOpen = value;
+        }
+
+        public DelegateCommand OpenFilterCommand => _filialBorderViewModel.OpenFilterCommand;
+
+        public DelegateCommand ApplyFilterCommand => _filialBorderViewModel.ApplyFilterCommand;
+
     }
 }
