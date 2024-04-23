d7a13aac694ab71efa812ce7444fd21e51b74625
Akimov Roman <frbhjvf@yandex.ru>
2024-04-23 22:59:30 +0300

    Подключены SQLite и Entity Framework
    - Подключено все для работы с бд
    - настроены конфигурации и контекст базы данных
    - сама база данных ещё не добавлена!

---
+++ Data/OrganisationsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using SQLite.CodeFirst;
+
+namespace CityOrganisations.Data
+{
+    /// <summary>
+    /// Связывает приложение с базой данных
+    /// </summary>
+    class OrganisationsDbContext : DbContext
+    {
+        public OrganisationsDbContext() : base() { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<OrganisationsDbContext>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+
+            var model = modelBuilder.Build(Database.Connection);
+            ISqlGenerator sqlGenerator = new SqliteSqlGenerator();
+            _ = sqlGenerator.Generate(model.StoreModel);
+        }
+    }
+}
---
+++ Data/OrganisationsDbContextInitializer.cs
@@ -0,0 +1,20 @@
+﻿using SQLite.CodeFirst;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityOrganisations.Data
+{
+    class OrganisationsDbContextInitializer : SqliteDropCreateDatabaseAlways<OrganisationsDbContext>
+    {
+        public OrganisationsDbContextInitializer(DbModelBuilder modelBuilder) : base(modelBuilder) { }
+
+        protected override void Seed(OrganisationsDbContext context)
+        {
+
+        }
+    }
+}
