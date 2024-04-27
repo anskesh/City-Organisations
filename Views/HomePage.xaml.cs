d8cb88d9f71585dff13337eda8f9e6f04d5b7832
Akimov Roman <frbhjvf@yandex.ru>
2024-04-27 13:30:01 +0300

    Реализация карты через Canvas

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -13,30 +13,5 @@ namespace CityOrganisations.Views
         {
             InitializeComponent();
         }
-
-        private void Border_MouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            if (sender is Border border)
-            {
-                double currentScaleX = border.LayoutTransform.Value.M11;
-                double currentScaleY = border.LayoutTransform.Value.M22;
-
-                /*
-                e.Delta - изменение колеса мыши. Если e.Delta положительное, то это значит,
-                что колесо было прокручено вверх, а если отрицательное, то вниз
-                */
-                double scaleChange = e.Delta > 0 ? 0.1 : -0.1;
-
-                double newScaleX = Math.Max(0.1, currentScaleX + scaleChange);
-                double newScaleY = Math.Max(0.1, currentScaleY + scaleChange);
-
-                border.LayoutTransform = new ScaleTransform(newScaleX, newScaleY);
-
-                double newWidth = MapBorder.ActualWidth + newScaleX;
-                double newHeight = MapBorder.ActualHeight + newScaleY;
-                MapBorder.Width = newWidth;
-                MapBorder.Height = newHeight;
-            }
-        }
     }
 }
