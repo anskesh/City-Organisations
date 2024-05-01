33b51d720649b0a0533f5c3aac0eae000d84c321
Akimov Roman <frbhjvf@yandex.ru>
2024-05-01 09:52:17 +0300

    Функционал добавления точек на карте

--- ViewModels/HomePageViewModel.cs
+++ ViewModels/HomePageViewModel.cs
@@ -2,6 +2,11 @@
 using CityOrganisations.Services.DataBase;
 using Prism.Events;
 using Prism.Services.Dialogs;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System;
 
 namespace CityOrganisations.ViewModels
 {
@@ -18,6 +23,42 @@ namespace CityOrganisations.ViewModels
         protected override bool OnSaveAdditionalCheck()
         {
             throw new System.NotImplementedException();
+        }
+
+        public void HandleMouseRightButton(MouseButtonEventArgs e, Canvas mapCanvas)
+        {
+            Point mousePosition = e.GetPosition(mapCanvas);
+
+            ContextMenu contextMenu = new ContextMenu();
+
+            MenuItem addItem = new MenuItem { Header = "Добавить" };
+            addItem.Click += (s, args) =>
+            {
+                AddPointToCanvas(mousePosition, mapCanvas);
+            };
+
+            contextMenu.Items.Add(addItem);
+
+            contextMenu.IsOpen = true;
+
+            contextMenu.PlacementTarget = mapCanvas;
+            contextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.MousePoint;
+        }
+
+
+        public void AddPointToCanvas(Point position, Canvas mapCanvas)
+        {
+            Image pointImage = new Image
+            {
+                Source = new BitmapImage(new Uri(@"C:\fork\City-Organisations\Images\Point.png")),
+                Width = 20,
+                Height = 20
+            };
+
+            Canvas.SetLeft(pointImage, position.X - pointImage.Width / 2);
+            Canvas.SetTop(pointImage, position.Y - pointImage.Height / 2);
+
+            mapCanvas.Children.Add(pointImage);
         }
     }
 }
--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using CityOrganisations.ViewModels;
+using System;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows;
 
 namespace CityOrganisations.Views
 {
@@ -12,6 +14,15 @@ namespace CityOrganisations.Views
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        private void MouseRightButtonCanvas(object sender, MouseButtonEventArgs e)
+        {
+            var viewModel = DataContext as HomePageViewModel;
+            if (viewModel != null)
+            {
+                viewModel.HandleMouseRightButton(e, MapCanvas);
+            }
         }
     }
 }
