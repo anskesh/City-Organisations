02b5984372a9f680e77869cf5aa1f8bcce0c5f2f
Akimov Roman <frbhjvf@yandex.ru>
2024-05-14 23:50:38 +0300

    ПРиближение и отдаление Canvas

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -1,16 +1,15 @@
 ﻿using CityOrganisations.ViewModels;
-using System;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows;
 
 namespace CityOrganisations.Views
 {
     public partial class HomePage : Page
     {
+        private double _scale = 1.0;
+
         public HomePage()
         {
             InitializeComponent();
@@ -20,7 +19,19 @@ namespace CityOrganisations.Views
         {
             var viewModel = DataContext as HomePageViewModel;
             viewModel.HandleMouseRightButton(e, MapCanvas);
+        }
 
+        private void MapScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var viewModel = (HomePageViewModel)DataContext;
+
+            int delta = e.Delta > 0 ? 1 : -1;
+
+            _scale += delta * 0.1;
+
+            e.Handled = true;
+
+            MapCanvas.LayoutTransform = new ScaleTransform(_scale, _scale);
         }
     }
 }
